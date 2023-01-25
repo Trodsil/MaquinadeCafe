579ed38ac09bbeed9e994459a9212737db3f08d1
Tiago R <tiagoalfonsorodriguezsilva@gmail.com>
2023-01-25 20:29:38 +0100

    commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import clases.Controller;
+
+public class Main {
+    public static void main(String[] args) {
+
+        new Controller();
+    }
+}
---
+++ src/clases/otros/Cafe.java
@@ -0,0 +1,15 @@
+package clases.otros;
+
+public class Cafe {
+    private String nombre;
+    private int gCafe;
+    private int gLeche;
+    private int gCacao;
+
+    public Cafe(String nombre, int gCafe, int gLeche, int gCacao) {
+        this.nombre = nombre;
+        this.gCafe = gCafe;
+        this.gLeche = gLeche;
+        this.gCacao = gCacao;
+    }
+}
